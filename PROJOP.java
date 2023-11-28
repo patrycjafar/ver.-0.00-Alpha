0a1be075453484a105510d4ce816306ce5379f93
patrycjafar <149515421+patrycjafar@users.noreply.github.com>
2023-11-28 19:21:19 +0100

    Update PROJOP.java

--- PROJOP.java
+++ PROJOP.java
@@ -1,43 +1,66 @@
-package zadania;
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
-public class kalkulator {
-    public static <string> void main(String[] args) {
+public class Kalkulator {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Podaj liczbe: ");
-        int liczba = scanner.nextInt();
+        // Wprowadzenie pierwszej liczby
+        System.out.println("Podaj pierwsza liczbe: ");
+        int pierwszaLiczba;
+        try {
+            pierwszaLiczba = scanner.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("Wprowadzono nieprawidłowe dane. Wprowadź liczbę całkowitą.");
+            return;
+        }
 
-        System.out.println("Podaj znak: ");
-        String znak = scanner.next();
+        // Wprowadzenie operatora matematycznego
+        System.out.println("Podaj operator (+, -, *, /): ");
+        String operator = scanner.next();
 
+        // Wprowadzenie drugiej liczby
         System.out.println("Podaj druga liczbe: ");
-        int druga = scanner.nextInt();
+        int drugaLiczba;
+        try {
+            drugaLiczba = scanner.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("Wprowadzono nieprawidłowe dane. Wprowadź liczbę całkowitą.");
+            return;
+        }
 
+        // Obliczenia na podstawie operatora
         System.out.println("Wynik:");
         int wynik;
 
-        boolean cofnięcie;
-
-
-
-        switch (znak){
-            case "+" :
-                wynik = liczba + druga;
+        switch (operator) {
+            case "+":
+                // Dodawanie
+                wynik = pierwszaLiczba + drugaLiczba;
                 System.out.println(wynik);
                 break;
-            case "-" :
-                wynik = liczba - druga;
+            case "-":
+                // Odejmowanie
+                wynik = pierwszaLiczba - drugaLiczba;
                 System.out.println(wynik);
                 break;
             case "*":
-                wynik = liczba * druga;
+                // Mnożenie
+                wynik = pierwszaLiczba * drugaLiczba;
                 System.out.println(wynik);
                 break;
             case "/":
-                wynik = liczba / druga;
-                System.out.println(wynik);
+                // Dzielenie zabezpieczone przed dzieleniem przez zero
+                if (drugaLiczba != 0) {
+                    wynik = pierwszaLiczba / drugaLiczba;
+                    System.out.println(wynik);
+                } else {
+                    System.out.println("Nie można dzielić przez zero.");
+                }
                 break;
+            default:
+                // Obsługa nieprawidłowego operatora
+                System.out.println("Nieprawidłowy operator.");
         }
     }
 }
